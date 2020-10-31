833a5f8008f11945de93e023c0d0862bb677cdab
Manuel Murillo <manuel@hermnasgazpachos.ca>
2020-10-31 17:47:07 -0400

    Initial commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className='my-1'>This is to keep your contacts represented by a set of cards.</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// Define routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 3936;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
