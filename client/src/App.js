9e3cc9773607837e3e087c0dfc934e3c5301779e
Qreceipt <57878939+wildcard69dev@users.noreply.github.com>
2023-01-01 13:56:16 -0500

    Corrective update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alerts from './components/layout/Alerts';
-import PrivateRoute from './components/routing/PrivateRoute';
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Alerts from './components/layout/Alerts'
+import PrivateRoute from './components/routing/PrivateRoute'
 
-import ContactState from './context/contact/ContactState';
-import AuthState from './context/auth/AuthState';
-import AlertState from './context/alert/AlertState';
-import './App.css';
+import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
+import './App.css'
 
 const App = () => {
   return (
@@ -35,7 +35,7 @@ const App = () => {
         </AlertState>
       </ContactState>
     </AuthState>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require('express')
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
 // Connect database
-connectDB();
+connectDB()
 
 // Init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
 // Define routes
-
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/auth', require('./routes/auth'))
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 
   app.get('/', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  )
 }
 
-const PORT = process.env.PORT || 3936;
+const PORT = process.env.PORT || 3936
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
